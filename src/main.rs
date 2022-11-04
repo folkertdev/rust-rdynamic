c9d2f24e4dbddf3343218672c1ed4a4de85ea600
Folkert <folkert@folkertdev.nl>
2022-11-04 22:11:18 +0100

    bigger example

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,58 @@
+use core::alloc::Layout;
+use core::ffi::c_void;
+use core::mem::MaybeUninit;
+use std::borrow::Borrow;
+use std::ffi::{CStr, OsStr};
+use std::fs::File;
+use std::io;
+use std::os::raw::c_char;
+use std::path::Path;
+use std::time::Duration;
+
 #[no_mangle]
-extern "C" fn look_at_me() -> usize {
-    42
+pub unsafe extern "C" fn roc_alloc(size: usize, _alignment: u32) -> *mut c_void {
+    libc::malloc(size)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn roc_realloc(
+    c_ptr: *mut c_void,
+    new_size: usize,
+    _old_size: usize,
+    _alignment: u32,
+) -> *mut c_void {
+    libc::realloc(c_ptr, new_size)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn roc_dealloc(c_ptr: *mut c_void, _alignment: u32) {
+    libc::free(c_ptr)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn roc_panic(c_ptr: *mut c_void, tag_id: u32) {
+    match tag_id {
+        0 => {
+            let slice = CStr::from_ptr(c_ptr as *const c_char);
+            let string = slice.to_str().unwrap();
+            eprintln!("Roc crashed with:\n\n\t{}\n", string);
+
+            std::process::exit(1);
+        }
+        _ => todo!(),
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn roc_memcpy(dst: *mut c_void, src: *mut c_void, n: usize) -> *mut c_void {
+    libc::memcpy(dst, src, n)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn roc_memset(dst: *mut c_void, c: i32, n: usize) -> *mut c_void {
+    libc::memset(dst, c, n)
 }
 
 fn main() {
-    println!("Hello, world!");
+    println!("hello world");
 }
